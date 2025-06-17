0dd62a9a2d3c97dfa103645e980e1e80db7be038
EC2 Default User <ec2-user@ip-172-31-12-218.ap-south-1.compute.internal>
2025-06-17 07:52:43 +0000

    initial comit

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-app.get('/',(req,res)=>{
-	res.send("Hello from express on EC2")
-});
-app.listen(3000,()=>{
-	console.log("Server running on localhost:3000");
-});
