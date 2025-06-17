3e85bbbb8dbe9cf4800d5707177f8cc11708a7af
AjaySusanth <ajaysusanth10@gmail.com>
2025-06-17 08:13:50 +0000

    minor change

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Express on EC2!');
+  res.send('Hello from Express on EC2');
 });
 
 app.listen(3000, () => {
   console.log('Server is running on localhost:3000');
 });
+
